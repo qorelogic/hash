4993ac7d104cb0c76507338505dea9ae654f2de5
Jeff Kiiza <kiiza@phuu.com>
2013-09-16 23:19:44 -0300

    small mod to js parse2api function

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 parsePlus500js2api() {
-	grep '^function ' plus500.js | perl -pe 's/.*function(.*) {/\1/g' > plus500.api.txt
+	grep '^function ' js/plus500.js  | \
+	perl -pe 's/.*function(.*) {/\1/g' > doc/plus500.api.txt
 }
