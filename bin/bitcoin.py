56430312b0a71877519be17b1a8e59918b6efe51
Jeff Kiiza <kiiza@phuu.com>
2013-11-20 18:48:16 -0300

    basecurrency bugfix

--- bin/bitcoin.py
+++ bin/bitcoin.py
@@ -76,15 +76,22 @@ class bitcoin:
 				   "Sign":self.sign}
 		domain = "btc-e.com"
 		
+		
+		# initialize self.currencies[i] if not a dictionary
+		try:
+			type(self.currencies[i])
+		except:
+			self.currencies[i] = {}
+			
 		try:
 			if type(['ftc'].index(i)) == type(1):
-				basec = 'btc'
+				self.currencies[i]['basecurr'] = 'btc'
 			else:
-				basec = 'usd'
+				self.currencies[i]['basecurr'] = 'usd'
 		except:
-			basec = 'usd'
+			self.currencies[i]['basecurr'] = 'usd'
 			
-		url = 'https://'+domain+"/api/2/"+str(i)+"_"+basec+"/ticker"
+		url = 'https://'+domain+"/api/2/"+str(i)+"_"+self.currencies[i]['basecurr']+"/ticker"
 		print '\t ticker \t' + url
 		print '\t---'
 		conn = httplib.HTTPSConnection(domain)
@@ -123,7 +130,7 @@ class bitcoin:
 		tusdbal = 0
 		tusdbalshould  = 0 
 
-		arr = {}
+		self.currencies = {}
 		# define portfolio weights
 		# register currency here and give it a portfolio weight
 		dat = {
@@ -131,7 +138,7 @@ class bitcoin:
 			'btc': [1,['19ANGDaYUTcb7zokc2cXd3espshu9ZfczC','1HKWHFLuv8UG1s2b8EsNXP935yAB8EH9Wg']],
 			#'btc': [1],
 			
-			# todo: add the currencies below
+			# todo: add the self.currencies below
 			'ftc':[0.1,[]],
 			#ppc
 			# trc
@@ -147,43 +154,49 @@ class bitcoin:
 		print ""
 		print "= Blockchains & Brokers ========================================================="
 		print ""
+		baseTicker = self.getTicker('btc')
 		for i in responseBTCe:
-			arr[i] = {}
-			arr[i]['xsell'] = 0
+			self.currencies[i] = {}
+			self.currencies[i]['xsell'] = 0
 			if responseBTCe[i] > 0 or dat.has_key(i):
 				print i
 				
-				ticker = self.getTicker(i)			
+				ticker = self.getTicker(i)
 				
 				try:
-					arr[i]['xsell'] = ticker['ticker']['sell']
+					self.currencies[i]['xsell'] = ticker['ticker']['sell']
 				except KeyError:
 					'stub'
+				try:
+					self.currencies[i]['xbasecurr'] = baseTicker['ticker']['sell']
+				except KeyError:
+					'stub'
+					
 			
 			# balance from b
 			bal = responseBTCe[i]
 			if bal > 0:
 				print '\t btce['+i+'] \t' + str(nd(bal,8)) + '\t '
 				print '\t---'
-			arr[i]['bal'] = bal
+			self.currencies[i]['bal'] = bal
 			
 			# litecoin balance
 			if i == 'ltc':
 				for j in range(0, len(dat[i][1])):
 					bal = self.getBlockChains("litecoinscout.com", "/address/"+dat[i][1][j], '(Balance: (.*?) LTC)')
-					arr[i]['bal'] += bal
+					self.currencies[i]['bal'] += bal
 
 			# bitcoin balance
 			try:
 				if i == 'btc' and type(dat[i][1]):
 					for j in range(0, len(dat[i][1])):
 						bal = self.getBlockChains("blockchain.info", "/address/"+dat[i][1][j], '(Balance.*?([\d\.]+) BTC)')
-						arr[i]['bal'] += bal
+						self.currencies[i]['bal'] += bal
 			except e:
 				print e
 			
-			arr[i]['usdbal'] = nv(arr[i]['bal'] * arr[i]['xsell'])
-			tusdbal += nv(arr[i]['usdbal'])
+			self.currencies[i]['usdbal'] = nv(self.currencies[i]['bal'] * self.currencies[i]['xsell'])
+			tusdbal += nv(self.currencies[i]['usdbal'])
 			
 				
 		# header
@@ -194,49 +207,55 @@ class bitcoin:
 		print "Coin \tBAL \t\tUSDBAL \t\tPercent \tSuggested % \tSell"
 
 		#body
-		for i in arr:
+		for i in self.currencies:
 			self.log('----------')
 			self.log('['+i+']')
 			try:
-				arr[i]['pcent'] = nv(arr[i]['usdbal'] / tusdbal * 100)
+				self.currencies[i]['pcent'] = nv(self.currencies[i]['usdbal'] / tusdbal * 100)
 			except ZeroDivisionError, e:
 				print e
 			
 			
-			self.log('usdbal['+i+']:'+str(arr[i]['usdbal'] ))
-			self.log('pcent['+i+']:'+str(arr[i]['pcent']))
+			self.log('usdbal['+i+']:'+str(self.currencies[i]['usdbal'] ))
+			self.log('pcent['+i+']:'+str(self.currencies[i]['pcent']))
 			pshould = 0
 			usdbalshould  = 0
 			try:		
 				pshould = nv(float(dat[i][0]) / tdat * 100)
 			except KeyError, e:
-				print e
+				''
 			try:
-				#usdbalshould = nv(arr[i]['usdbal'] / arr[i]['pcent'] * pshould)
+				#usdbalshould = nv(self.currencies[i]['usdbal'] / self.currencies[i]['pcent'] * pshould)
 				usdbalshould = nv(tusdbal * pshould / 100)
 			except ZeroDivisionError:
 				usdbalshould = nv(pshould)
 			self.log('pshould['+i+']:'+str(pshould))
-			self.log('usdbalshould['+i+']:'+str(usdbalshould))
+			self.log('usdbalshoulda['+i+']:'+str(usdbalshould))
 			tusdbalshould  += usdbalshould 
 			
-			try:
-				if arr[i]['bal'] > 0 or dat.has_key(i):
-					print i + ": \t" + str(arr[i]['bal']) + " \t" + str(arr[i]['usdbal']) + " \t" + str(arr[i]['pcent'])+"% \t\t" + str(pshould) + "% \t" + str(arr[i]['xsell'])
-					self.log('usdbal['+i+']:'+str(arr[i]['usdbal']))
-					self.log('usdbalshould['+i+']:'+str(usdbalshould))
-					if arr[i]['usdbal'] > usdbalshould:
-						decrease = arr[i]['usdbal'] - usdbalshould
-						print  '\t\t\t\t\t\t\t\t-'+ str(decrease/arr[i]['xsell']) + ' ' + str(i) + " ie. \t\t" + str(decrease)+' USD'
-						print  ""
-					if arr[i]['usdbal'] <= usdbalshould:
-						increase = usdbalshould - arr[i]['usdbal']
-						print  '\t\t\t\t\t\t\t\t+'+ str(increase/arr[i]['xsell']) + ' ' + str(i) + " ie. \t" + str(increase)+' USD'
-						print ""
+			#try:
+			if self.currencies[i]['bal'] > 0 or dat.has_key(i):
+				print i + ": \t" + str(self.currencies[i]['bal']) + " \t" + str(self.currencies[i]['usdbal']) + " \t" + str(self.currencies[i]['pcent'])+"% \t\t" + str(pshould) + "% \t" + str(self.currencies[i]['xsell'])
+				self.log('usdbal['+i+']:'+str(self.currencies[i]['usdbal']))
+				self.log('usdbalshouldb['+i+']:'+str(usdbalshould))
+				if i == 'ftc':
+					xrate = self.currencies[i]['xsell'] * self.currencies[i]['xbasecurr']
 				else:
-					'stub'
-			except:
-				'stub'
+					xrate = self.currencies[i]['xsell']
+				self.log('xrate:'+str(xrate))
+				self.log('xbasecurr:'+str(self.currencies[i]['xbasecurr']))
+				if self.currencies[i]['usdbal'] > usdbalshould:
+					decrease = self.currencies[i]['usdbal'] - usdbalshould
+					print  '\t\t\t\t\t\t\t\t-'+ str(decrease / xrate) + ' ' + str(i) + " ie. \t\t" + str(decrease)+' USD'
+					print  ""
+				if self.currencies[i]['usdbal'] <= usdbalshould:
+					increase = usdbalshould - self.currencies[i]['usdbal']
+					print  '\t\t\t\t\t\t\t\t+'+ str(increase / xrate) + ' ' + str(i) + " ie. \t" + str(increase)+' USD'
+					print ""
+			else:
+				''
+			#except Error, e:
+			#	print e
 
 		#footer
 		print 'Total USD: ' + str(tusdbal)
