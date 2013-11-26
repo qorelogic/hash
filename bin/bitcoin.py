678e7c2c0befcf1511e791990b9137604bceadf2
Jeff Kiiza <kiiza@phuu.com>
2013-11-26 08:09:56 -0300

    updated API keys

--- bin/bitcoin.py
+++ bin/bitcoin.py
@@ -34,8 +34,10 @@ class bitcoin:
 	
 	def __init__(self):
 		# Replace these with your own API key data
-		self.BTC_api_key = "JY83Z3PQ-IG1HMU08-ETL8Q3OW-NTQQDSZ8-COLT8K9Y"
-		self.BTC_api_secret = "865d33452b852df84f9dc0ddcfa6a5303bb453368f00f8b3a747e0f40686274d"
+		#self.BTC_api_key = "JY83Z3PQ-IG1HMU08-ETL8Q3OW-NTQQDSZ8-COLT8K9Y"
+		#self.BTC_api_secret = "865d33452b852df84f9dc0ddcfa6a5303bb453368f00f8b3a747e0f40686274d"
+		self.BTC_api_key = "0361F7UQ-RM8O1ULD-OQHVG5UE-NHUPLIBG-LD666U7J"
+		self.BTC_api_secret = "9df5c8a1250103f71f03b44f428c87726597fb27d40d7a065070ef3335401a00"
 		
 		self.logBuffer = ""
 		
