5af02c820ddd835c437a631fb6206b0df55759aa
Jeff Kiiza <kiizaa@gmail.com>
2014-03-06 18:31:23 -0300

    miner.sh running minerd

---
+++ bin/miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MINERD="/home/qore/Desktop/cpuminer-2.3.2/minerd"
+
+$MINERD -a scrypt -o http://p2pool.org:9327 -O LXZc3HdmDGmfhV4hMoxwtsg29jVozRSFCf:x --no-stratum
+#./minerd -a scrypt -o http://p2pool.org:22552 -O DBHAPfBRc7L7WTcr2Cnf6tfvTRtdKEpnkF:x --no-stratum
+
